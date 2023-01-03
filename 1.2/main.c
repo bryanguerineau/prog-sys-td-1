76140e4a13ac5657547faa4111b615a88b4ae618
Parallels <parallels@kali-linux-2022-2.localdomain>
2023-01-03 08:50:14 +0100

    Q1.2

---
+++ 1.2/main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void print_PCB()
+{
+    printf("\n PCB \t | \t");
+    printf("PPID: %d \n", getppid());
+    printf("\t \t PID: %d\n", getpid());
+    printf("\t \t UID: %d\n", getuid());
+    printf("\t \t GID: %d\n", getgid());
+    
+}
+
+int main() {
+    pid_t ret = fork();
+    wait();
+    printf("fork() has returned: %d\n", ret);
+    print_PCB();
+    exit(EXIT_SUCCESS);
+}
